485c05800cd336fe2521f0f469eb157117ff906d
Kasper Nielsen <kallenielsen87@live.dk>
2016-02-22 14:34:38 +0100

    Har tilføjet et par button klasser, tilføjet gamestates til Game1 filen, og sat Loginscreen til at være det første gamestate.

--- Working title/Working title/UI/InputFields/InputLogin.cs
+++ Working title/Working title/UI/InputFields/InputLogin.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Working_title.UI.InputFields
 {
