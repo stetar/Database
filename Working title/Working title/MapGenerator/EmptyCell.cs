861d9ba32c17d59f79f6800fc003e137cbfe6983
Philip Ullersted <philipullersted@hotmail.com>
2016-02-25 01:48:18 +0100

    Fixed a bugs with git not noticing EmptyCell

---
+++ Working title/Working title/MapGenerator/EmptyCell.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Working_title.MapGenerator
+{
+    public class EmptyCell : Cell
+    {
+        public EmptyCell(Vector2 position, Size size) : 
+            base(position,size)
+        {
+            
+        }
+    }
+}
